cee16565112d45de9b3b64c5e8b89973aa60670c
satyavardhan026 <23471-cm-026@pace.ac.in>
2026-04-26 14:45:43 +0530

    Delete javapractice directory

--- javapractice/satya1874.java
+++
@@ -1,9 +0,0 @@
-https://www.youtube.com/shorts/RfkROhrY3KU?feature=share
-<html>
-<head>
-<title>WEB PAGE</title>
-</head>
-<body>
-<a href="https://www.javatpoint.com/java-tutorial"> <img src="C:\deepika\javapoint.png"></a>
-</body>
-</html>
